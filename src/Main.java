f07dd4933c1da74b9c214c6a2dd8841ce6c3cd84
mradawiec <milosz523@o2.pl>
2023-01-21 16:09:37 +0100

    2 task - fixed

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,13 @@ public class Main {
         pet.takeForAWalk();
         pet.takeForAWalk();*/
 
+        Human Marek = new Human();
 
         Car car = new Car("BMW","E36", "Nardo Grey", 1.6);
         car.newRide();
+        Marek.car = car;
+
+
+
     }
 }
